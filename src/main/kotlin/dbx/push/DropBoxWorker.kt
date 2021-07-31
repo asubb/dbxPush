240b002504dd392a256259fbbd32b02e74b129b8
asubb <a.a.subb@gmail.com>
2021-07-31 08:22:02 -0400

    [better upload]

--- src/main/kotlin/dbx/push/DropBoxWorker.kt
+++ src/main/kotlin/dbx/push/DropBoxWorker.kt
@@ -2,32 +2,48 @@ package dbx.push
 
 import com.dropbox.core.DbxRequestConfig
 import com.dropbox.core.v2.DbxClientV2
-import com.dropbox.core.v2.files.UploadErrorException
-import com.dropbox.core.v2.files.WriteMode
+import com.dropbox.core.v2.fileproperties.PropertyGroup
+import com.dropbox.core.v2.files.*
+import net.jodah.failsafe.CircuitBreaker
+import net.jodah.failsafe.Failsafe
+import net.jodah.failsafe.RetryPolicy
+import net.jodah.failsafe.function.CheckedRunnable
+import java.io.ByteArrayInputStream
+import java.io.Closeable
 import java.io.File
 import java.io.FileInputStream
+import java.net.SocketTimeoutException
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+import java.util.*
 import java.util.concurrent.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.random.Random
+
 
 class DropBoxWorker(
-        private val client: DbxClientV2
-) {
+    private val client: DbxClientV2,
+    threadsCount: Int
+) : Closeable {
 
     lateinit var diff: Diff
     lateinit var localRootPath: String
     lateinit var remoteRootPath: String
 
-    private val pool = Executors.newFixedThreadPool(1)
+    private val pool = Executors.newFixedThreadPool(threadsCount)
 
     companion object {
-        fun create(clientIdentifier: String, accessToken: String): DropBoxWorker {
+        fun create(clientIdentifier: String, accessToken: String, threadsCount: Int): DropBoxWorker {
             val config = DbxRequestConfig
-                    .newBuilder(clientIdentifier)
-                    .build()
+                .newBuilder(clientIdentifier)
+                .build()
             val client = DbxClientV2(config, accessToken)
 
             val account = client.users().currentAccount!!
             println("Logged in as ${account.name.displayName}")
-            return DropBoxWorker(client)
+            return DropBoxWorker(client, threadsCount)
         }
     }
 
@@ -39,6 +55,7 @@ class DropBoxWorker(
     }
 
     fun uploadFiles() {
+        val counter = AtomicInteger(0);
         val misplaced2 = diff.findMisplaced()
         if (misplaced2.isNotEmpty()) {
             println("Can't sync, too many misplaced:")
@@ -50,13 +67,14 @@ class DropBoxWorker(
 
 
         val toUpload = ConcurrentLinkedQueue<File>(diff.getStrict())
+        counter.addAndGet(toUpload.size)
         val tasks = ArrayList<Future<Boolean>>()
         while (toUpload.size > 0) {
 
             val fileToUpload: File? = toUpload.poll()
             if (fileToUpload != null) {
                 tasks += pool.submit(Callable<Boolean> {
-                    uploadFile(fileToUpload, localRootPath, client, remoteRootPath)
+                    uploadFile(fileToUpload, localRootPath, client, remoteRootPath, counter)
                 })
             }
 
@@ -65,10 +83,8 @@ class DropBoxWorker(
 
         // wait for everything to finish
         val allSucceeded = tasks
-                .map(Future<Boolean>::get)
-                .all { it }
-
-        pool.shutdown()
+            .map(Future<Boolean>::get)
+            .all { it }
 
         if (allSucceeded || tasks.size == 0)
             diff.finalize()
@@ -78,56 +94,132 @@ class DropBoxWorker(
         val misplaced = diff.findMisplaced()
         if (misplaced.isNotEmpty()) {
             misplaced.asSequence()
-                    .filter { it.value.size == 1 } // only leave the ones we are confident about
-                    .forEach {
-                        val relativeDir = File(it.value[0].name).toRelativeString(File(remoteRootPath))
-                        val ref = it.key.ref ?: throw IllegalStateException("${it.key} is inappropriate")
-                        println("Trying to move ${it.key} to $relativeDir")
-                        // make sure dir exists
-                        val components = relativeDir.split(File.separator)
-                        components.subList(0, components.size - 1)
-                                .forEachIndexed { index, _ ->
-                                    val dir = File(localRootPath
-                                            + File.separator
-                                            + components.subList(0, index + 1).joinToString(File.separator)
-                                    )
-                                    if (!dir.exists()) {
-                                        println("Creating subdir `$dir`")
-                                        dir.mkdirs()
-                                    }
-                                }
-                        println("Renaming to ${localRootPath + File.separator + relativeDir}")
-                        if (!ref.renameTo(File(localRootPath + File.separator + relativeDir))) {
-                            throw IllegalStateException("Can't move files")
+                .filter { it.value.size == 1 } // only leave the ones we are confident about
+                .forEach {
+                    val relativeDir = File(it.value[0].name).toRelativeString(File(remoteRootPath))
+                    val ref = it.key.ref ?: throw IllegalStateException("${it.key} is inappropriate")
+                    println("Trying to move ${it.key} to $relativeDir")
+                    // make sure dir exists
+                    val components = relativeDir.split(File.separator)
+                    components.subList(0, components.size - 1)
+                        .forEachIndexed { index, _ ->
+                            val dir = File(
+                                localRootPath
+                                        + File.separator
+                                        + components.subList(0, index + 1).joinToString(File.separator)
+                            )
+                            if (!dir.exists()) {
+                                println("Creating subdir `$dir`")
+                                dir.mkdirs()
+                            }
                         }
+                    println("Renaming to ${localRootPath + File.separator + relativeDir}")
+                    if (!ref.renameTo(File(localRootPath + File.separator + relativeDir))) {
+                        throw IllegalStateException("Can't move files")
                     }
+                }
             diff.rereadLocal()
         }
     }
 
-    private fun uploadFile(fileToUpload: File, localRootPath: String, client: DbxClientV2, remoteRootPath: String): Boolean {
+    private fun uploadFile(
+        fileToUpload: File,
+        localRootPath: String,
+        client: DbxClientV2,
+        remoteRootPath: String,
+        counter: AtomicInteger
+    ): Boolean {
         val relativePath = fileToUpload.toRelativeString(File(localRootPath))
         val tid = "${Thread.currentThread().id}_${System.nanoTime() % 1024}"
-        println("[$tid] Uploading file ${fileToUpload.name} to $remoteRootPath/$relativePath")
+        val filePath = "$remoteRootPath/$relativePath"
+        val fileAttr = Files.readAttributes(fileToUpload.toPath(), BasicFileAttributes::class.java)
+        println("[$tid] Uploading file ${fileToUpload.name} to $filePath")
         FileInputStream(fileToUpload).use { stream ->
-            try {
-                // upload file
-                if (fileToUpload.length() > 100 * 1024 * 1024) {
-                    TODO("dropbox API doesn't support uploading large files without using sessions")
-                } else {
-                    val metadata = client.files()
-                            .uploadBuilder("$remoteRootPath/$relativePath")
-                            .withMode(WriteMode.OVERWRITE)
-                            .uploadAndFinish(stream)
-                    println("[$tid] Uploaded `${metadata.name}` ${metadata.size} bytes ${metadata.contentHash}")
-                    return true
-                }
+            return try {
+                val retryPolicy = RetryPolicy<Any>()
+                    .handle(com.dropbox.core.NetworkIOException::class.java)
+                    .handle(com.dropbox.core.RateLimitException::class.java)
+                    .withBackoff(3, 30, ChronoUnit.SECONDS, 3.0)
+                    .withJitter(0.1)
+                    .withMaxRetries(10)
+                    .onRetry {
+                        println("[$tid][Retrying] attemptCount=${it.attemptCount}, elapsedTime=${it.elapsedTime}, lastResult=${it.lastResult}")
+                    }
+                Failsafe.with(retryPolicy)
+                    .run(CheckedRunnable {
+                        val metadata = doUpload(fileToUpload, client, stream, filePath, fileAttr, tid, counter)
+                        println("[$tid, left=${counter.decrementAndGet()}] Uploaded `${metadata.name}` ${metadata.size} bytes ${metadata.contentHash}")
+                        Thread.sleep(Random.nextLong(300)) // wait for up to 300ms to avoid rate limiting
+                    })
+                true
             } catch (e: UploadErrorException) {
                 val stackTrace = e.stackTrace.joinToString("\n")
-                System.err.println("[$tid] ${e.message}\n $stackTrace")
+                System.err.println("[$tid, left=${counter.decrementAndGet()}] ${e.message}\n $stackTrace")
+                false
             }
-            return false
         }
     }
 
+    private fun doUpload(
+        fileToUpload: File,
+        client: DbxClientV2,
+        stream: FileInputStream,
+        filePath: String,
+        fileAttr: BasicFileAttributes,
+        tid: String,
+        counter: AtomicInteger
+    ): FileMetadata {
+        // upload the file
+        return if (fileToUpload.length() > 100 * 1024 * 1024) {
+            var offset = 0L
+            val session = client.files()
+                .uploadSessionStart(false)
+                .finish()
+            val buf = ByteArray(5 * 1024 * 1024)
+            do {
+                // read
+                val amount = stream.read(buf)
+                if (amount > 0) {
+                    // flush
+                    client.files()
+                        .uploadSessionAppendV2(
+                            UploadSessionCursor(
+                                session.sessionId,
+                                offset
+                            )
+                        )
+                        .uploadAndFinish(ByteArrayInputStream(buf, 0, amount))
+                    offset += amount
+                }
+            } while (amount != -1)
+
+            // close
+            client.files()
+                .uploadSessionFinish(
+                    UploadSessionCursor(
+                        session.sessionId,
+                        offset
+                    ),
+                    CommitInfo(
+                        filePath,
+                        WriteMode.OVERWRITE,
+                        false,
+                        Date.from(fileAttr.creationTime().toInstant()),
+                        false,
+                        emptyList<PropertyGroup>(),
+                        false
+                    )
+                )
+                .finish()
+        } else {
+            client.files()
+                .uploadBuilder(filePath)
+                .withMode(WriteMode.OVERWRITE)
+                .uploadAndFinish(stream)
+        }
+    }
+
+    override fun close() {
+        pool.shutdownNow()
+    }
 }
--- src/main/kotlin/dbx/push/Push.kt
+++ src/main/kotlin/dbx/push/Push.kt
@@ -8,20 +8,37 @@ import java.io.PrintWriter
 import java.util.*
 import kotlin.system.exitProcess
 
+const val defaultThreadsCount = 15
+
 val t = Option("t", "access-token", true, "Token to access DropBox API")
 val c = Option("c", "client-identifier", true, "Client Identifier to access DropBox API")
-val l = Option("l", "local-path", true, "Path to folder on local machine to make comparison against, i.e. /users/me/Pictures")
+val l = Option(
+    "l",
+    "local-path",
+    true,
+    "Path to folder on local machine to make comparison against, i.e. /users/me/Pictures"
+)
 val r = Option("r", "remote-path", true, "Path to folder inside DropBox to make comparison against, i.e. /photos")
 val s = Option("s", "skip-list", true, "Comma-separate list of files to be skipped")
-val a = Option("a", "action", true, "What to do during that run:\n" + Action.values().joinToString("\n") { it.id + " - " + it.description })
+val a = Option(
+    "a",
+    "action",
+    true,
+    "What to do during that run:\n" + Action.values().joinToString("\n") { it.id + " - " + it.description })
 val h = Option("h", "help", false, "Prints this help")
+val threads = Option(null, "threads", true, "Amount of threads to use to upload files, by default $defaultThreadsCount")
 
 val options = Options().of(t, c, l, r, a, s, h)
 
 enum class Action(val id: String, val description: String) {
-    RESTRUCTURE("restructure", "Restructure local files that correspond to remote files based on the remote " +
-            "folder structure. The one that has no pair will remain untouched."),
-    UPLOAD("upload", "Upload current local structure to remote folder. It'll overwrite remote files with new one if it's not the same locally."),
+    RESTRUCTURE(
+        "restructure", "Restructure local files that correspond to remote files based on the remote " +
+                "folder structure. The one that has no pair will remain untouched."
+    ),
+    UPLOAD(
+        "upload",
+        "Upload current local structure to remote folder. It'll overwrite remote files with new one if it's not the same locally."
+    ),
     PREFETCH("prefetch", "Prefetch all files into local cache only"),
     CLEAN_LOCAL("clean-local", "Clean cache: local files structure"),
     CLEAN_DBX("clean-dbx", "Clean cache: dropbox files structure"),
@@ -53,39 +70,41 @@ fun main(args: Array<String>) {
     val accessToken = properties.getProperty("access-token") ?: cli.getRequired(t) { it }
     val clientIdentifier = properties.getProperty("client-identifier") ?: cli.getRequired(c) { it }
     val localRootPath = (properties.getProperty("local-path") ?: cli.getRequired(l) { it })
-            .let { File(it).absolutePath }
+        .let { File(it).absolutePath }
     val remoteRootPath = properties.getProperty("remote-path") ?: cli.getRequired(r) { it }
     val skipList = (properties.getProperty("skip-list") ?: cli.get(s) { it })
-            ?.let { it.split(",").map { it.trim() }.toSet() }
-            ?: emptySet()
+        ?.let { it.split(",").map { it.trim() }.toSet() }
+        ?: emptySet()
     val action = cli.getRequired(a) { v -> Action.values().first { it.id == v } }
+    val threadsCount = cli.get(threads) { it.toInt() } ?: defaultThreadsCount
 
-    val client = DropBoxWorker.create(clientIdentifier, accessToken)
-
-    when (action) {
-        Action.RESTRUCTURE -> {
-        println("Restructuring local files to conform remote")
-            client.fetchDiff(localRootPath, remoteRootPath, skipList)
+    DropBoxWorker.create(clientIdentifier, accessToken, threadsCount).use { client ->
+        when (action) {
+            Action.RESTRUCTURE -> {
+                println("Restructuring local files to conform remote")
+                client.fetchDiff(localRootPath, remoteRootPath, skipList)
                     .restructureLocal()
-        }
-        Action.UPLOAD -> {
-        println("Starting upload current structure into cloud")
-            client.fetchDiff(localRootPath, remoteRootPath, skipList)
+            }
+            Action.UPLOAD -> {
+                println("Starting upload current structure into cloud")
+                client.fetchDiff(localRootPath, remoteRootPath, skipList)
                     .uploadFiles()
-        }
-        Action.PREFETCH -> client.fetchDiff(localRootPath, remoteRootPath, skipList)
-        Action.CLEAN_LOCAL -> TODO()
-        Action.CLEAN_DBX -> TODO()
-        Action.CONFIG -> {
-            println("Storing values to config")
-            properties.setProperty("access-token", accessToken)
-            properties.setProperty("client-identifier", clientIdentifier)
-            properties.setProperty("local-path", localRootPath)
-            properties.setProperty("remote-path", remoteRootPath)
-            properties.setProperty("skip-list", skipList.joinToString(","))
-            properties.store(FileWriter(propertiesFile), "DropBox Push configuration file")
+            }
+            Action.PREFETCH -> client.fetchDiff(localRootPath, remoteRootPath, skipList)
+            Action.CLEAN_LOCAL -> TODO()
+            Action.CLEAN_DBX -> TODO()
+            Action.CONFIG -> {
+                println("Storing values to config")
+                properties.setProperty("access-token", accessToken)
+                properties.setProperty("client-identifier", clientIdentifier)
+                properties.setProperty("local-path", localRootPath)
+                properties.setProperty("remote-path", remoteRootPath)
+                properties.setProperty("skip-list", skipList.joinToString(","))
+                properties.store(FileWriter(propertiesFile), "DropBox Push configuration file")
+            }
         }
     }
+
 }
 
 private fun printHelp() {
